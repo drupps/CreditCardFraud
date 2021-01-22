a002ca95bc2d2e82eaa4636bd91999202daa9c0f
Han Zhao <han.zhao1989@gmail.com>
2021-01-21 20:50:19 -0600

    initial change

--- demo-template/app.py
+++ demo-template/app.py
@@ -1,13 +1,71 @@
-from flask import Flask, jsonify, render_template
+import numpy as np
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 
 app = Flask(__name__)
 engine = create_engine("sqlite:///data/cities.sqlite")
 
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+ccfraud = Base.classes.ccfraud
+
+
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/intro")
+def map():
+    return render_template("intro.html")
+
+
+@app.route("/data")
+def scatter():
+    return render_template("data.html")
+
+
+@app.route("/code")
+def charts():
+    return render_template("code.html")
+
+
+@app.route("/graphs")
+def charts():
+    return render_template("graphs.html")
+
+
+@app.route("/setup")
+def charts():
+    return render_template("setup.html")
+
+
+@app.route("/data/<x>")
+def pov(x):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all transactions
+    results = session.query(Happiness.ISO2, Happiness.ISO3, Happiness.Country, Happiness.Region, Happiness.Score,
+                            Happiness.GDP, Happiness.Social_Support, Happiness.Life_Exp, Happiness.Freedom_Choice,
+                            Happiness.Generosity, Happiness.Corruption, Happiness.Year).filter(Happiness.Year == x).all()
+    print(results)
+
+    session.close()
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
