f4046e783d1623ca67219c28fe8a5a818bcd8a2f
Dave Ruppel <drupps@gmail.com>
2021-01-21 20:10:52 -0600

    schema update

--- schema.sql
+++ schema.sql
@@ -1,12 +1,8 @@
---DROP TABLE IF EXISTS time_real, v_real, amount_real, fraud_real, time_synthetic, type_synthetic, transactions_synthetic, fraud_synthetic
+--DROP TABLE IF EXISTS real, synthetic
 
-CREATE TABLE "time_real" (
+CREATE TABLE "real" (
     id SERIAL PRIMARY KEY,
-    time INT NOT NULL
-)
-
-CREATE TABLE "v_real" (
-    id SERIAL PRIMARY KEY,
+    time INT NOT NULL,
     v1 DECIMAL NOT NULL,
     v2 DECIMAL NOT NULL,
     v3 DECIMAL NOT NULL,
@@ -35,41 +31,22 @@ CREATE TABLE "v_real" (
     v26 DECIMAL NOT NULL,
     v27 DECIMAL NOT NULL,
     v28 DECIMAL NOT NULL,
-)
-
-CREATE TABLE "amount_real" (
-    id SERIAL PRIMARY KEY,
     amount float NOT NULL,
-)
-
-CREATE TABLE "fraud_real" (
-    id SERIAL PRIMARY KEY,
     class int NOT NULL
 )
 
-CREATE TABLE "time_synthetic" (
-    id serial primary key,
-    step int NOT NULL
-)
 
-CREATE TABLE "type_synthetic" (
+CREATE TABLE "synthetic" (
     id serial primary key,
+    step int NOT NULL,
     type varchar(25) NOT NULL,
-    amount float not null
-)
-
-CREATE TABLE "transactions_synthetic"(
-    id serial primary key not null,
+    amount float not null,
     name_origin varchar(30),
     old_origin_balance float,
     new_origin_balance float,
     name_destination varchar(30),
     old_destination_balance float,
-    new_destination_balane float
-)
-
-CREATE TABLE "fraud_synthetic" (
-    id serial primary key,
+    new_destination_balane float,
     is_fraud int,
     is_fraud_flagged int
 )
