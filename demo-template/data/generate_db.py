38719a9a1280da05dd682343eaf23aca1fecbad2
Dave Ruppel <drupps@gmail.com>
2021-01-27 14:55:22 -0600

    removing old data from previous project

--- demo-template/data/generate_db.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-cities = pd.read_csv("cities.csv")
-engine = create_engine("sqlite:///cities.sqlite")
-cities.to_sql(name='cities', con=engine, if_exists='replace', index=False)
